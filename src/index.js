c36c83249c12af3af813e78c15a7e80f8f91cae1
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-16 16:27:23 -0300

    Write logic for displaying lists and todos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import "./style.css";
-import dateFormatter from "./dateFormatter";
-import createList, { lists, deleteList } from "./todoAppController";
+import dateFormatter from "./modules/dateFormatter"
+import createList, { lists, deleteList } from "./modules/classes/listClass";
+import { renderLists } from "./modules/renderDOM";
 
+let defaultTodoDeleted = false;
+let defaultTodoCreated = false;
+
+function addDefaultTodo() {
+    if (defaultTodoCreated || defaultTodoDeleted) return;
+
+    const defaultList = createList("Default");
+    defaultList.addTodo("Create a new to-do!", "Press on the button that is somewhere in this page to create a new to-do task.", dateFormatter(16, 5, 2025), "normal");
+    defaultTodoCreated = true;
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    addDefaultTodo();
+    renderLists(lists);
+})
