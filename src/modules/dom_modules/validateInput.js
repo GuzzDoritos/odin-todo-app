3e07611d93b195cff3ee343737c20045a074bb1d
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-18 15:48:57 -0300

    Change function to use priority constant variables

--- src/modules/dom_modules/validateInput.js
+++ src/modules/dom_modules/validateInput.js
@@ -1,7 +1,9 @@
+import { HIGH, LOW } from "../classes/todoClass";
+
 export default function validateInput(value, type) { //type: text, date or priority
     let isValueValid;
     if (type === "priority") {
-        isValueValid = value < 3 && value >= 0;
+        isValueValid = value < HIGH && value >= LOW;
     }
     if (!isValueValid) return 0;
     else return 1;
