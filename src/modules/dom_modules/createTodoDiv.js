fefc484e64bd4db834b7e14a2290db4837d95930
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-20 11:10:31 -0300

    Move createTodoDiv to its own module file

---
+++ src/modules/dom_modules/createTodoDiv.js
@@ -0,0 +1,24 @@
+import { differenceInDays, format } from "date-fns";
+
+export default function createTodoDiv(todo) {
+    const todoDiv = document.createElement("div");
+    todoDiv.className = "todo-div";
+
+
+    const todoName = document.createElement("h3");
+    const todoDescription = document.createElement("p");
+    const todoDate = document.createElement("p");
+    const openTodo = document.createElement("button");
+
+    todoName.textContent = todo.title;
+    todoDescription.textContent = todo.description;
+    todoDate.textContent = format(todo.dueDate, "dd/MM/yyyy") + " - " + "in " + (differenceInDays(todo.dueDate, new Date()) + 1) + " day(s).";
+    openTodo.textContent = "Open Todo";
+
+    todoDiv.appendChild(todoName);
+    todoDiv.appendChild(todoDescription);
+    todoDiv.appendChild(todoDate);
+    todoDiv.appendChild(openTodo);
+
+    return todoDiv;
+}
