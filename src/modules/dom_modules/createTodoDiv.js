927c1797b57647b92c7f123b25ce424510f7e45a
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-20 11:48:15 -0300

    Add toggle done button and refactor todo div

--- src/modules/dom_modules/createTodoDiv.js
+++ src/modules/dom_modules/createTodoDiv.js
@@ -4,12 +4,20 @@ export default function createTodoDiv(todo) {
     const todoDiv = document.createElement("div");
     todoDiv.className = "todo-div";
 
+    const buttonSideDiv = document.createElement("div")
+    buttonSideDiv.className = "todo-div-button-side";
+
     const markAsDoneButton = document.createElement("input");
     markAsDoneButton.type = "checkbox";
     markAsDoneButton.className = "mark-as-done-btn";
     markAsDoneButton.addEventListener("click", () => {
         todo.toggleDone();
     })
+
+    buttonSideDiv.appendChild(markAsDoneButton);
+
+    const textSideDiv = document.createElement("div");
+    textSideDiv.className = "todo-div-text-side";
 
     const todoName = document.createElement("h3");
     const todoDescription = document.createElement("p");
@@ -21,11 +29,9 @@ export default function createTodoDiv(todo) {
     todoDate.textContent = format(todo.dueDate, "dd/MM/yyyy") + " - " + "in " + (differenceInDays(todo.dueDate, new Date()) + 1) + " day(s).";
     openTodo.textContent = "Open Todo";
 
-    todoDiv.appendChild(markAsDoneButton);
-    todoDiv.appendChild(todoName);
-    todoDiv.appendChild(todoDescription);
-    todoDiv.appendChild(todoDate);
-    todoDiv.appendChild(openTodo);
+    textSideDiv.append(todoName, todoDescription, todoDate, openTodo);
+
+    todoDiv.append(buttonSideDiv, textSideDiv)
 
     return todoDiv;
 }
