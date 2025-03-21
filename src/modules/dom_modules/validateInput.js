1031b617fbc3216969c5319ee50687eeac72d3f8
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-21 13:15:03 -0300

    Fix smol bug from priority validation

--- src/modules/dom_modules/validateInput.js
+++ src/modules/dom_modules/validateInput.js
@@ -3,7 +3,7 @@ import { HIGH, LOW } from "../classes/todoClass";
 export default function validateInput(value, type) { //type: text, date or priority
     let isValueValid;
     if (type === "priority") {
-        isValueValid = value < HIGH && value >= LOW;
+        isValueValid = value <= HIGH && value >= LOW;
     }
     if (!isValueValid) return 0;
     else return 1;
