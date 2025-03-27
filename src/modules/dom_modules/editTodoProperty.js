a2e6dfa7758494ce2342ed25f08f693adb3446c3
Gustavo Bragada <gustavo.bragada@hotmail.com>
2025-03-27 17:58:25 -0300

    Implement editing todo module

---
+++ src/modules/dom_modules/editTodoProperty.js
@@ -0,0 +1,86 @@
+import { format } from "date-fns";
+import { displayList } from "../renderDOM";
+import { setInnerPropValue, showExpandedTodo } from "./expandTodo";
+import dateFormatter from "../dateFormatter";
+import { createPriorityInput } from "./createInput";
+
+export default function editTodoProperty(propValueDiv, prop, todo, list, expandedTodoDiv) {
+    propValueDiv.replaceChildren();
+
+    let inputBox = document.createElement("input");
+    inputBox.className = "edit-input";
+
+    const configureInputBox = () => {
+        switch (prop) {
+            case `title`:
+            case `description`:
+                inputBox.type = "text";
+                inputBox.value = todo[prop];
+                break;
+            case `dueDate`:
+                inputBox.type = "date";
+                inputBox.value = format(todo[prop], "yyyy-MM-dd");
+                // propValueDiv.append(inputBox);
+                break;
+            case `priority`:
+                inputBox = createPriorityInput().selectInput;
+                inputBox.className = "edit-input";
+                // propValueDiv.append(inputBox.input);
+                break;
+        }
+    }
+
+    configureInputBox();
+
+    const okButton = document.createElement("button");
+    okButton.className = "edit-ok-button";
+    okButton.textContent = "OK";
+
+    propValueDiv.append(inputBox, okButton);
+
+    inputBox.focus();
+    if (prop === `dueDate`) inputBox.showPicker();
+    if (prop !== `priority`) inputBox.select();
+
+
+    okButton.addEventListener("click", submit);
+    inputBox.addEventListener("keydown", (e) => {if(e.key === "Enter") submit()});
+
+    function submit() {
+        let newValue;
+        if (inputBox.value.trim() === "" && prop === `description`) {
+            newValue = "No description provided";
+        } else if (inputBox.value.trim() === "") {
+            return;
+        } else {
+            newValue = getNewValue();
+        }
+        function getNewValue() {
+            switch(prop) {
+                case `title`:
+                case `description`:
+                    return inputBox.value;
+                case `dueDate`:
+                    const formattedDate = dateFormatter(inputBox.value);
+                    return formattedDate;
+                case `priority`:
+                    return Number.parseInt(inputBox.value);
+            }
+        } 
+        todo[prop] = newValue;
+        // propValueDiv.textContent = "";        
+        // const propValueHolder = document.createElement("div");
+        // propValueHolder.className = "prop-value-holder";
+        
+        // const innerPropValue = setInnerPropValue(prop, todo);
+
+        // propValueHolder.innerHTML = `${innerPropValue} <box-icon type='solid' name='edit-alt'></box-icon>`;
+
+        // propValueDiv.append(propValueHolder);
+
+        // propValueHolder.addEventListener("click", () => editTodoProperty(propValueDiv, prop, todo, list));
+        displayList(list);
+        showExpandedTodo(expandedTodoDiv);
+        console.log(todo);
+    }
+}
