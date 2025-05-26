16f4c0ef563f3bfcdecf10502889d87cd2d7e068
GuzzDoritos <69866522+GuzzDoritos@users.noreply.github.com>
2025-05-26 23:27:54 +0000

    Start implementing feature to re-add isDone property to todo object

--- src/modules/local_storage/populateStorage.js
+++ src/modules/local_storage/populateStorage.js
@@ -6,8 +6,11 @@ export default function populateStorage() {
     const savedData = JSON.parse(localStorage.getItem(USER_DATA));
     savedData.forEach((list) => {
         const savedList = createList(list.name, list.id);
-        list.todoList.forEach(todo => {
+        list.todoList.forEach((todo, index) => {
             savedList.addTodo(todo.title, todo.description, todo.dueDate, todo.priority, todo.id);
+            if (todo.hasOwnProperty("isDone")) {
+                savedList[index].isDone = todo.isDone;
+            }
         })
     })
 
