297df3c171015035d804c5a9c0e89de701410871
Guzz <gustavo.bragada@hotmail.com>
2025-06-24 22:36:22 -0300

    Change the due message so it makes more sense

--- src/modules/dom_modules/createTodoDiv.js
+++ src/modules/dom_modules/createTodoDiv.js
@@ -6,6 +6,9 @@ export default function createTodoDiv(todo, list) {
     const todoDiv = document.createElement("div");
     todoDiv.classList = todo.isDone ? "todo-div todo-done" : "todo-div";
 
+    const todoDivHeader = document.createElement("div");
+    todoDivHeader.className = "todo-div-header";
+
     const buttonSideDiv = document.createElement("div")
     buttonSideDiv.className = "todo-div-button-side";
 
@@ -27,14 +30,23 @@ export default function createTodoDiv(todo, list) {
     const todoDate = document.createElement("p");
 
     todoName.textContent = todo.title;
-    todoDate.textContent = format(todo.dueDate, "dd/MM/yyyy") + " - " + "in " + (differenceInDays(todo.dueDate, new Date()) + 1) + " day(s).";
+    // todoDate.textContent = format(todo.dueDate, "dd/MM/yyyy") + " - " + "in " + (differenceInDays(todo.dueDate, new Date()) + 1) + " day(s).";
+    const remainingDays = differenceInDays(todo.dueDate, new Date().setHours(0, 0, 0, 0));
+    if (remainingDays > 0) {
+        todoDate.textContent = `${format(todo.dueDate, "dd/MM/yyyy")} - due ${remainingDays > 1 ? `in ${differenceInDays(todo.dueDate, new Date()) + 1} days` : "tomorrow"}.`;
+    } else if (remainingDays == 0) {
+        todoDate.textContent = `${format(todo.dueDate, "dd/MM/yyyy")} - due today.`
+    } else {
+        todoDate.textContent = `${format(todo.dueDate, "dd/MM/yyyy")} - overdue by ${remainingDays * -1} day${remainingDays * -1 > 1 ? "s" : ""}!`
+    }
 
     textSideDiv.append(todoName, todoDate);
 
     const expandedTodoDiv = createExpandedTodoDiv(todo, list);
 
-    todoDiv.append(buttonSideDiv, textSideDiv, expandedTodoDiv)
 
+    todoDivHeader.append(buttonSideDiv, textSideDiv);
+    todoDiv.append(todoDivHeader, expandedTodoDiv);
 
     todoDiv.addEventListener("click", (event) => {
         const exclude = ["mark-as-done-btn", "prop-value-holder", "edit-input", "edit-ok-button", "priority-display"];
